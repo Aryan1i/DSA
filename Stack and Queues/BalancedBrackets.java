fe9c1c8450f9e601cf7eaeb0d53c5ef9e261c433
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2025-12-08 00:33:01 +0530

    Balanced Brackets
    Implement a program to check if brackets in an expression are balanced.

---
+++ Stack and Queues/BalancedBrackets.java
@@ -0,0 +1,75 @@
+//Problem
+
+  /*1. You are given a string exp representing an expression.
+    2. You are required to check if the expression is balanced i.e. closing brackets and opening brackets match up well.
+    
+    e.g.
+    [(a + b) + {(c + d) * (e / f)}] -> true
+    [(a + b) + {(c + d) * (e / f)]} -> false
+    [(a + b) + {(c + d) * (e / f)} -> false
+    ([(a + b) + {(c + d) * (e / f)}] -> false
+    Input Format
+    A string str
+    Output Format
+    true or false
+    Constraints
+    0 <= str.length <= 100
+*/
+
+//Solution
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalancedBracket {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '{' || ch == '[' || ch == '(') {
+                st.push(ch);
+            } else if (ch == ')') {
+                boolean val = HandleClosing(st, '(');
+                if (val == false) {
+                    System.out.println(false);
+                    return;
+                }
+            } else if (ch == '}') {
+                boolean val = HandleClosing(st, '{');
+                if (val == false) {
+                    System.out.println(false);
+                    return;
+                }
+            } else if (ch == ']') {
+                boolean val = HandleClosing(st, '[');
+                if (val == false) {
+                    System.out.println(false);
+                    return;
+                }
+            }
+        }
+
+        if (st.size() == 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        return;
+    }
+
+    public static boolean HandleClosing(Stack<Character> st, char correspondCh) {
+        if (st.size() == 0) {
+            return false;
+        } else if (st.peek() != correspondCh) {
+            return false;
+        } else {
+            st.pop();
+            return true;
+        }
+    }
+}

fa6cffe93f655cebc6c28e6486950229436f8451
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-01-25 02:52:56 +0530

    Maximum of Generic Tree
    Implemented a method to find the maximum value in a generic tree structure.

---
+++ Generic Tree/maxOfGenericTree.java
@@ -0,0 +1,60 @@
+//Problem
+
+//Maximum of Generic Tree
+
+//Soution
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+public class GenericTreeConstrctor {
+
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static int max(Node node){
+        if(node==null){
+            return 0;
+        }
+
+        int max=node.data;
+
+        for(Node child:node.children){
+            int temp=max(child);
+            if(temp>max){
+                max=temp;
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int [] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
+
+        Node root=null;
+        Stack<Node> st = new Stack<>();
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==-1){
+                st.pop();
+            }else{
+                Node  t = new Node();
+                t.data=arr[i];
+
+                if(st.size()>0){
+                    st.peek().children.add(t);
+                }else{
+                    root=t;
+                }
+                st.push(t);
+            }
+        }
+      
+        System.out.println(max(root));
+    }
+}

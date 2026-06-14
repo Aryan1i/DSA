1ffa3860f3d9787d1c5a6b43fcd5127b6c3ca75f
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-06-14 18:54:20 +0530

    Quick Left Rotation
    Implemented a solution for left-rotating an array by k indexes, including handling cases where k exceeds the array size.

---
+++ Array/QuickLeftRotation.java
@@ -0,0 +1,45 @@
+//Problem
+
+    /*Given an array, arr[] of positive elements, and an integer k, the task is to left-rotate the array k indexes.
+    Note: Rotate the array even if the k is greater than the size of the array. In these cases after every full array rotation, the array comes the same as the original array.
+    
+    Examples:
+    
+    Input: arr[] = [1, 2, 3, 4, 5, 6, 7], k = 2 
+    Output: [3, 4, 5, 6, 7, 1, 2]
+    Explanation: Rotating the above array by 2 will make the output array.
+    Input: arr[] = [1, 2, 3, 4, 5, 6],  k = 12
+    Output: [1, 2, 3, 4, 5, 6]
+    Explanation: left Rotation of above array 12 times gives same array as output. 
+    Input: arr[] = [1, 2, 3, 4, 5, 6],  k = 11
+    Output: [6, 1, 2, 3, 4, 5]
+    Explanation: left Rotation of above array 11 times & in resultant output 6 comes to the statring position.
+    Expected Time Complexity: O(n)
+    Expected Auxiliary Space: O(1)
+    
+    Constraints:
+    1 ≤ arr.size, arr[i] ≤ 105
+    0 ≤ k ≤ 109
+    
+    */
+
+//Solution
+
+class Solution {
+    void leftRotate(int arr[], int k) {
+        k = k % arr.length;
+        reverse(arr, 0 , k - 1);
+        reverse(arr, k , arr.length-1);
+        reverse(arr, 0 , arr.length - 1);
+    }
+    
+    void reverse(int arr[],int l , int r){
+        while(l < r){
+            int temp = arr[r];
+            arr[r] = arr[l];
+            arr[l] = temp;
+            l++;
+            r--;
+        }
+    }
+}

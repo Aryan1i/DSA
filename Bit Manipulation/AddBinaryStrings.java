640eb13c8d7eedb35bfdde359e4d736b616a5d9c
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-06-16 22:41:19 +0530

    sum two binary strings
    Implement a method to add two binary strings and return the result without leading zeros.

---
+++ Bit Manipulation/AddBinaryStrings.java
@@ -0,0 +1,57 @@
+//Problem
+
+/*Given two binary strings s1 and s2 consisting of only 0s and 1s. Find the resultant string after adding the two Binary Strings.
+Note: The input strings may contain leading zeros but the output string should not have any leading zeros.
+
+Input: s1 = "1101", s2 = "111"
+Output: 10100
+Explanation:
+ 1101
++ 111
+10100
+Input: s1 = "00100", s2 = "010"
+Output: 110
+Explanation: 
+  100
++  10
+  110
+Constraints:
+1 ≤s1.size(), s2.size()≤ 106*/
+
+//Solution
+
+class Solution {
+    public String addBinary(String s1, String s2) {
+        StringBuilder ans = new StringBuilder();
+        int carry = 0;
+        
+        int i = s1.length() - 1; 
+        int j = s2.length() - 1;
+        
+        while(i >= 0 || j >= 0 || carry == 1){
+            int temp = carry;
+            
+            if(i >= 0){
+                temp += s1.charAt(i) - '0';
+                i--;
+            }
+            
+            if(j >= 0){
+                temp += s2.charAt(j) - '0';
+                j--;
+            }
+            
+            ans.append(temp%2);
+            carry = temp/2;
+        }
+        
+        ans.reverse();
+        
+        int k = 0;
+        while (k < ans.length() - 1 && ans.charAt(k) == '0') {
+            k++;
+        }
+        
+        return ans.substring(k);
+    }
+}

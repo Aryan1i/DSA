81713bab65f8916c2e070018c4ce933fea6eecb6
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-06-25 22:01:24 +0530

    Check Perfect Square
    Implement a method to check if a number is a perfect square.

---
+++ Math/ProductOfPrimes.java
@@ -0,0 +1,26 @@
+//Prbolem
+
+    /*Given a positive integer n, return true if it is a perfect square, otherwise return false. A number is called a perfect square if it can be written in the form a × a, where a is an integer.
+    
+    Examples:
+    
+    Input: n = 35
+    Output: false 
+    Explanation: 35 is not a perfect square.
+    Input: n = 49
+    Output: true
+    Explanation: 49 is a perfect square (7 * 7 = 49).
+    Constraints:
+    1 ≤ n ≤ 105*/
+
+
+class Solution {
+    public boolean isPerfectSquare(int n) {
+        double x = Math.sqrt(n);
+        int y = (int) Math.sqrt(n);
+        
+        double z = x - y;
+        
+        return z == 0;
+    }
+}

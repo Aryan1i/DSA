4c70f91692adc5dc46f2f59e2c838907a8ab1208
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-07-27 15:53:29 +0530

    Squares in a Matrix

---
+++ 2D Array/Squares in a Matrix.java
@@ -0,0 +1,32 @@
+//Problem
+
+/*Given a mxn matrix, count the number of squares in the matrix.
+
+Examples:
+
+Input: m = 2, n = 2
+Output: 5
+Explanation: There are a total of 5 squares in a 2*2 matrix. Four 1*1 squares and one 2*2 square.
+Input: m = 4, n = 3
+Output: 20
+Explanation: There are a total of 20 squares in a 4*3 matrix. 
+Constraints:
+1 <= m,n <= 104
+
+*/
+
+//Solution
+
+class Solution {
+    public int squaresInMatrix(int m, int n) {
+        int limit = Math.min(m,n);
+        
+        int ans = 0;
+        
+        for(int i = 1; i <= limit; i++){
+            ans += (m - i + 1) * (n - i + 1);
+        }
+        
+        return ans;
+    }
+};

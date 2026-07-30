503be523ad64d518243e6c39f1a3c7b6b2d0f3b7
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-07-30 21:21:19 +0530

    Linked List End Insertion

---
+++ Linked List/Linked List End Insertion.java
@@ -0,0 +1,46 @@
+//Problem
+    
+    /*You are given the head of a Singly Linked List and a value x, insert that value x at the end of the LinkedList and return the head of the modified Linked List.
+    
+    Examples :
+    
+    Input: x = 6,
+       
+    Output: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+    Explanation: We can see that 6 is inserted at the end of the linkedlist.
+       
+    Input: x = 1,
+       
+    Output: 4 -> 5 -> 1
+    Explanation: We can see that 1 is inserted at the end of the linked list.
+          
+    Constraints:
+    0 ≤ number of nodes ≤ 105
+    0 ≤ node->data , x ≤ 103*/
+
+//Solution
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+        if(head == null) return new Node(x);
+        Node oriHead = head;
+        while(head.next != null){
+            head = head.next;
+        }
+        
+        head.next = new Node(x);
+        
+        return oriHead;
+    }
+}

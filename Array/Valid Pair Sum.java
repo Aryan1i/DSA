63cab4db97c5efe02212fa553b8c288540eca314
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-08-01 10:35:49 +0530

    Valid Pair Sum

---
+++ Array/Valid Pair Sum.java
@@ -0,0 +1,49 @@
+//Problem
+    
+    /*Given an array of size N, find the number of distinct pairs {i, j} (i != j) in the array such that the sum of a[i] and a[j] is greater than 0.
+    
+    Example 1:
+    
+    Input: N = 3, a[] = {3, -2, 1}
+    Output: 2
+    Explanation: {3, -2}, {3, 1} are two 
+    possible pairs.
+    Example 2:
+    
+    Input: N = 4, a[] = {-1, -1, -1, 0}
+    Output: 0
+    Explanation: There are no possible pairs.
+    Your Task:  
+    You don't need to read input or print anything. Complete the function ValidPair() which takes the array a[ ] and size of array N as input parameters and returns the count of such pairs.
+    
+    Expected Time Complexity: O(N * Log(N))
+    Expected Auxiliary Space: O(1)
+    
+    Constraints:
+    1 ≤ N ≤ 105 
+    -104  ≤ a[i] ≤ 104*/
+
+//Solution
+
+class Solution {
+    static long ValidPair(int a[], int n) {
+        if(n == 1) return 0;
+        Arrays.sort(a);
+        
+        int i = 0;
+        int j = n - 1;
+        
+        long count = 0;
+        
+        while(i < j){
+            if(a[i] + a[j] > 0){
+                count += j - i;
+                j--;
+            } else {
+                i++;
+            }
+        }
+        
+        return count;
+    }
+}

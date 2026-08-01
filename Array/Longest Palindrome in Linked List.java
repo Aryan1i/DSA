01e39a0177f36ddf5711b2a576d7fb82b2822c0a
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-08-01 10:43:41 +0530

    Longest Palindrome in Linked List

---
+++ Array/Longest Palindrome in Linked List.java
@@ -0,0 +1,21 @@
+//Problem
+    
+    /*Given a linked list, find length of the longest palindrome list that exists in the given linked list.
+    
+    Examples:
+    
+    Input: Linked List: 2->3->7->3->2->12->24
+    Output: 5
+    Explanation: 2 -> 3 -> 7 -> 3 -> 2 is the linked list whose nodes leads to a palindrome.
+    Input: Linked List: 12->4->4->3->14
+    
+    Output: 2
+    Explanation: 4 -> 4 is the linked list whose nodes leads to a palindrome with length 2.
+    Constraints:
+    1 ≤ number of nodes ≤ 104
+    1 ≤ data of nodes ≤ 105
+    
+    */
+
+//Solution
+

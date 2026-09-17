1f02ce3a6e2716226176a463e0489345a81f1802
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-09-17 21:29:43 +0530

    Count Leaves in Binary Tree

---
+++ Binary Tree/Count Leaves in Binary Tree.java
@@ -0,0 +1,48 @@
+//Problem
+    
+    /*Given a Binary Tree of size n, You have to count leaves in it. For example, there are two leaves in the following tree
+    
+    
+     
+    
+    Examples:
+    
+    Input:
+    Given Tree is  
+    
+    Output: 3
+    Explanation: Three leaves are 3, 5 and 1.
+    Input:
+    Given Tree is
+    
+    Output: 4
+    Explanation: Four leaves are 20, 40, 65 and 80.
+    Input:
+    Given Tree is  
+    
+    Output: 3
+    Explanation: Three leaves are 20, 27 and 40.
+     Constraints:
+    1<= number of nodes <= 105
+    1<= node->data <= 105*/
+
+//Solution
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+}*/
+
+class Solution {
+    int countLeaves(Node node) {
+        if(node == null) return 0;
+        if(node.left == null && node.right == null) return 1;
+        
+        int l = countLeaves(node.left);
+        int r = countLeaves(node.right);
+        
+        return l + r;
+    }
+}
